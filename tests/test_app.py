41d15452e5835a091bb3bd9950928d76ad277823
Eliezer Capuano <jeliezercapuanolopez@gmail.com>
2024-12-22 11:01:04 -0400

    feat: :test_tube: ADDING FAILING TEST FOR CHECKING REVERT COMMIT

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,4 +5,4 @@ def test_home_message(client: FlaskClient):
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.json == {"message": "hello world"}
+    assert response.json == {"message": "hello world 2"}
